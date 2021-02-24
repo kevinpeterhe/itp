304764bac41d254b373b4f7ce226c26ee69b7685
kevinpeterhe <49597431+kevinpeterhe@users.noreply.github.com>
2021-02-24 13:03:45 -0500

    cleaned up connected wk1 code

--- ConnectedDevices/01_HTML/ArduinoSendJSON/ArduinoSendJSON.ino
+++ ConnectedDevices/01_HTML/ArduinoSendJSON/ArduinoSendJSON.ino
@@ -1,26 +1,39 @@
-/*
-  JSON serial sending
-  Reads sensors and formats them as a JSON object, then sends
-  the resulting object as a JSON string out the serial port.
-  Uses the following library:
-  https://librarymanager/All#Arduino_JSON
-  Circuit:
-  - Pushbutton connected to pin D2 and to ground
-  - Potentiometer connected to pin A0
-
-  created 12 Jan 2021
-  by Tom Igoe
-*/
+//import libraries
 #include <Arduino_JSON.h>
+#include <ezButton.h>
 
-// set the input pins:
-const int buttonPin = 2;
+//variables for led pins
+int ledPin3 = 3;
+int ledPin2 = 5;
+int ledPin1 = 6;
+int val = 1;
+//variables for time thresholds
+const int PRESS_TIME_1  = 1000; 
+const int PRESS_TIME_2  = 2000; 
+const int PRESS_TIME_3  = 3000; 
 
-// variables to hold previous button and knob states:
-int lastButtonState = HIGH;
-int lastKnobState = 0;
-// threshold of change for the knob (range 0-100):
-int threshold = 3;
+// create ezButton object that attach to pin 7;
+ezButton button(7);
+
+//variables for button states
+unsigned long pressedTime  = 0;
+unsigned long releasedTime = 0;
+bool isPressing = false;
+bool is1Detected = false;
+bool is2Detected = false;
+bool is3Detected = false;
+
+// variables to hold previous led states:
+int ledState1 = LOW;
+int ledState2 = LOW;
+int ledState3 = LOW;
+int lastledState1 = LOW;
+int lastledState2 = LOW;
+int lastledState3 = LOW;
+
+//variables for serial handshake
+String handshake;
+int h;
 
 // if the inputs have changed:
 bool inputsChanged = false;
@@ -28,40 +41,137 @@ bool inputsChanged = false;
 JSONVar outgoing;
 
 void setup() {
-  // initialize serial and set input pin modes:
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT_PULLUP);
+  button.setDebounceTime(50); // set debounce time to 50 milliseconds
+  pinMode(ledPin1, OUTPUT);  // declare LED as output
+  pinMode(ledPin2, OUTPUT);  // declare LED as output
+  pinMode(ledPin3, OUTPUT);  // declare LED as output
 
   // initialize values in JSON object:
-  outgoing["knob"] = 0;
-  outgoing["button"] = 0;
+  outgoing["pressed"] = 1;
+  outgoing["led1"] = 0;
+  outgoing["led2"] = 0;
+  outgoing["led3"] = 0;
 }
 
-void loop() {
-  // read inputs, map if needed:
-  int knobState = analogRead(A0);
-  knobState = map(knobState, 0, 1023, 0, 100);
-  int buttonState = digitalRead(buttonPin);
+void loop(){
+  button.loop(); // MUST call the loop() function first
 
-  // if knob has changed:
-  if (abs(knobState - lastKnobState) > threshold) {
-    outgoing["knob"] = knobState;
-    // save knob state for comparison next time:
-    lastKnobState = knobState;
+  //button pressed down
+  if(button.isPressed()){
+    outgoing["pressed"] = 1;
+    pressedTime = millis();
+    isPressing = true;
+    is1Detected = false;
+    is2Detected = false;
+    is3Detected = false;
+  }
+
+  //button is released
+  if(button.isReleased()) {
+    outgoing["pressed"] = 0;
+    Serial.println(outgoing);
+    //if detect serial data from HTML
+    if (Serial.available() > 0) {
+      handshake  = Serial.readStringUntil('\n');
+      if (handshake = 'h') {
+    //TURN OFF LEDs
+        delay(2000);
+        digitalWrite(ledPin3, LOW);  // turn LED OFF
+        ledState3 = LOW;
+        check();
+        send();
+        delay(1000);
+        digitalWrite(ledPin2, LOW);
+        ledState2 = LOW;
+        check();
+        send();
+        delay(1000);
+        digitalWrite(ledPin1, LOW);
+        ledState1 = LOW;
+        check();
+        send();
+      }
+    }
+    isPressing = false;
+    releasedTime = millis();
+    long pressDuration = releasedTime - pressedTime;
+    //if press time is shorter than 1 second, no LEDs are turned on
+    if( pressDuration < PRESS_TIME_1) {
+      Serial.println("Button was pressed for less than 1 second");
+    }
+  }
+
+  if(isPressing == true && is1Detected == false) {
+    long pressDuration = millis() - pressedTime;
+    // if press duration is between 1 and 2 seconds
+    if( pressDuration >= PRESS_TIME_1 && pressDuration < PRESS_TIME_2) {
+      //turn on LED1
+      digitalWrite(ledPin1, HIGH);
+      ledState1 = HIGH;
+      check();
+      send();
+//      Serial.println("Button was pressed for more than 1 second");
+      is1Detected = true;
+    }
+  }
+  // if press duration is between 2 and 3 seconds
+  if(isPressing == true && is2Detected == false) { 
+    long pressDuration = millis() - pressedTime;
+    
+    if( pressDuration >= PRESS_TIME_2 && pressDuration < PRESS_TIME_3) {
+      //turn on LED2
+      digitalWrite(ledPin2, HIGH);
+      ledState2 = HIGH;
+      check();
+      send();
+//      Serial.println("Button was pressed for more than 2 seconds");
+      is2Detected = true;
+    }
+  }
+  // if press duration is longer than 3 seconds
+  if(isPressing == true && is3Detected == false) {
+    long pressDuration = millis() - pressedTime;
+    if( pressDuration > PRESS_TIME_3 ) {
+      //turn on LED3
+      digitalWrite(ledPin3, HIGH);
+      ledState3 = HIGH;
+      check();
+      send();
+//      Serial.println("Button was pressed for more than 3 seconds");
+      is3Detected = true;
+    }
+  }
+}
+
+//function to check LED state
+void check(){
+  // if the led state has changed:
+  if (ledState3 != lastledState3) {
+    outgoing["led3"] = ledState3;
+    // save led state state for comparison next time:
+    lastledState3 = ledState3;
     // set change flag so serial will send:
     inputsChanged = true;
   }
-  
-  // if the button has changed:
-  if (buttonState != lastButtonState) {
-    outgoing["button"] = buttonState;
-    // save button state for comparison next time:
-    lastButtonState = buttonState;
+  if (ledState2 != lastledState2) {
+    outgoing["led2"] = ledState2;
+    // save led state for comparison next time:
+    lastledState2 = ledState2;
     // set change flag so serial will send:
     inputsChanged = true;
   }
+  if (ledState1 != lastledState1) {
+    outgoing["led1"] = ledState1;
+    // save led state for comparison next time:
+    lastledState1 = ledState1;
+    // set change flag so serial will send:
+    inputsChanged = true;
+  }
+}
 
-  // if either sensor has changed, send it:
+// if the LED has been lit, send it:
+void send(){
   if (inputsChanged) {
     Serial.println(outgoing);
     // clear the change flag:
--- ConnectedDevices/01_HTML/script.js
+++ ConnectedDevices/01_HTML/script.js
@@ -1,4 +1,4 @@
-//colors
+//colors of fire states
 let color0 = "#900C3F";
 let color1 = "#C70039";
 let color2 = "#FF5733";
@@ -19,19 +19,14 @@ let lastButtonState1 = 0;
 let lastButtonState2 = 0;
 let lastButtonState3 = 0;
 
+//variables for timer event
 let rect1;
 let timerID;
 let counter = 0;
-
 let pressHoldEvent = new CustomEvent("pressHold");
-
-// Increase or decreae value to adjust how long
-// one should keep pressing down before the pressHold
-// event fires
 let pressHoldDuration = 100;
-
-// let item = document.getElementById("rect3");
 let item = document.querySelector("#rect3");
+
 
 // this function is called when the page is loaded. 
 // element event listeners are  added here:
@@ -51,7 +46,6 @@ function setup(event) {
 
   background = document.getElementById('bg');
 
-
   // initialize the serialport object:
   serial = new p5.SerialPort(); // new instance of the serialport library
   serial.on('list', printList); // callback function for serialport list event
@@ -66,7 +60,7 @@ function setup(event) {
 }
 
 
-
+//functions to set button states
 function setPowerState1() {
   // change its value, depending on its current value:
   if (powerButton1.value == 'on') {
@@ -117,6 +111,7 @@ function setPowerState3() {
 //   thisSpan.innerHTML = currentValue;
 // }
 
+
 // make a serial port selector object:
 function printList(portList) {
   // create a select object:
@@ -143,6 +138,7 @@ function openPort() {
 }
 
 
+//function to process incoming JSON data
 function serialEvent() {
   // read a line of incoming data:
   var inData = serial.readLine();
@@ -152,14 +148,19 @@ function serialEvent() {
     // button value:
     // if the button's changed and it's pressed, take action:
     if (sensors.led1 !== lastButtonState1) {
+      //if its high
       if (sensors.led1 === 1) {
+        //set button state
         setPowerState1(sensors.led1);
-        // bg.style.backgroundColor = color1;
+        //show rectangle and set color
         document.getElementById("rect1").style.backgroundColor = color1;
         document.getElementById("rect1").style.visibility = "visible";
       }
+      //if its low
       if (sensors.led1 === 0) {
+        //set button state
         setPowerState1(sensors.led1);
+        //hide rectangle and set color
         document.getElementById("rect1").style.backgroundColor = color0;
         document.getElementById("rect1").style.visibility = "hidden";
       }
@@ -168,13 +169,19 @@ function serialEvent() {
     }
     // if the button's changed and it's pressed, take action:
     if (sensors.led2 !== lastButtonState2) {
+      //if its high
       if (sensors.led2 === 1) {
+        //set button state
         setPowerState2(sensors.led2);
+        //show rectangle and set color
         document.getElementById("rect2").style.backgroundColor = color2;
         document.getElementById("rect2").style.visibility = "visible";
       }
+      //if its low
       if (sensors.led2 === 0) {
+        //set button state
         setPowerState2(sensors.led2);
+        //hide rectangle and set color
         document.getElementById("rect2").style.backgroundColor = color1;
         document.getElementById("rect2").style.visibility = "hidden";
       }
@@ -183,28 +190,23 @@ function serialEvent() {
     }
       // if the button's changed and it's pressed, take action:
       if (sensors.led3 !== lastButtonState3) {
+        //if its high
         if (sensors.led3 === 1) {
+          //set button state
           setPowerState3(sensors.led3);
+          //show rectangle and set color
           document.getElementById("rect3").style.backgroundColor = color3;
           document.getElementById("rect3").style.visibility = "visible";
+          //trigger timer event
           pressingDown();
           doSomething();
         }
-        // if (sensors.pressed === 0) {
-        //   setPowerState3(sensors.led3);
-        //   document.getElementById("rect3").style.backgroundColor = color2;
-        //   document.getElementById("rect3").style.visibility = "hidden";
-        //   // document.getElementById("rect3").addEventListener("mouseup", notPressingDown, false);
-        //   // document.getElementById("rect3").addEventListener("mouseleave", notPressingDown, false);
-        //   serial.write('h');
-        // }
         // save button value for next time:
         lastButtonState3 = sensors.led3;
       }
       console.log("sensors" + sensors.pressed);
-      if (sensors.pressed === 0) {
-        // pressingDown();
-
+      //if its low and previously turned on
+      if (sensors.pressed === 0 && sensors.led3 === 1) {
         notPressingDown();
         setPowerState3(sensors.led3);
       }
@@ -214,59 +216,41 @@ function serialEvent() {
   }
 }
 
+
 // add a listener for the page to load:
 window.addEventListener('DOMContentLoaded', setup);
 
 
+//function to turn off final rectangle and send serial data to Arduino
 function turnoff() {
-
   document.getElementById("rect3").style.backgroundColor = color2;
   document.getElementById("rect3").style.visibility = "hidden";
   serial.write('h\n');
 }
 
+//function to record start of timer event
 function pressingDown(e) {
   // Start the timer
   requestAnimationFrame(timer);
-
   // e.preventDefault();
-
   console.log("Pressing!");
 }
 
+//function to cancel animation after timer runs out
 function notPressingDown(e) {
-  // // Stop the timer
-  // cancelAnimationFrame(timerID);
-  // counter = 0;
-
-  // rect3.style.transform = "scale(" + 1 + ", " + 1 + ")";
   requestAnimationFrame(timer2);
-
-  // timerID = requestAnimationFrame(timer);
-  // // console.log("falling timerID" + timerID);
-  // counter--;
-  // // console.log("falling counter" + counter);
-  // x = 2 - counter / 100;
-  // rect3.style.transform = "scale(" + 1 + ", " + x + ")";
-
+  timerID2 = requestAnimationFrame(timer2);
   cancelAnimationFrame(timerID2);
-  // counter = 0;
   console.log("Not pressing!");
 }
 
-//
-// Runs at 60fps when you are pressing down
-//
+// Runs at 60fps when you are pressing down, function to run counter for 1 sec
 function timer() {
   console.log("counter: " + counter);
-
   if (counter < pressHoldDuration) {
     timerID = requestAnimationFrame(timer);
-    // console.log(timerID);
     counter++;
-
     x = 1 + counter / 100;
-    // console.log("rising counter" + x);
     rect3.style.transform = "scale(" + 1 + ", " + x + ")";
   } else {
     console.log("Press threshold reached!");
@@ -274,16 +258,13 @@ function timer() {
   }
 }
 
+// Runs at 60fps when you are pressing down, function to run reverse counter for 1 sec, then turning off rectangle
 function timer2() {
   console.log("counter: " + counter);
-
   if (counter > 0) {
     timerID2 = requestAnimationFrame(timer2);
-    // console.log(timerID);
     counter--;
-
     x = 1 + counter / 100;
-    console.log("x Val: " + x);
     rect3.style.transform = "scale(" + 1 + ", " + x + ")";
   } else if ( x === 1 ) {
     console.log("Zero reached!");
@@ -293,9 +274,7 @@ function timer2() {
   }
 }
 
+//function to log timer event started
 function doSomething(e) {
   console.log("pressHold event fired!");
 }
-
-// let scale = 1 + counter / 100;
-// rect3.style.transform = "scale(" + 1 + ", " + scale + ")";
